de36eba4ac03e7b755ce6f5b5c2ca8393925b6a7
Walter Machaca <waltermachaca884@.gmail.com>
2025-08-19 17:16:06 -0400

    Práctica 4 final

--- README.md
+++ README.md
@@ -7,39 +7,39 @@
 ## GENERACIÓN DE GRÁFICAS CON PYTHON 
 
 Gráfico1 Barras
-![Gráfico1 Barras](Output/grafica_1_barras.png)
+![Gráfico1 Barras](Generados/grafica_1_barras.png)
 
 Gráfico1 pastel 
-![Gráfico1 pastel](Output/grafica_1_pastel.png)
+![Gráfico1 pastel](Generados/grafica_1_pastel.png)
 
 Gráfico2 Barras
-![Gráfico2 Barras](Output/grafica_2_barras.png)
+![Gráfico2 Barras](Generados/grafica_2_barras.png)
 
 Gráfico2 pastel 
-![Gráfico2 pastel](Output/grafica_2_pastel.png)
+![Gráfico2 pastel](Generados/grafica_2_pastel.png)
 
 Gráfico3 Barras
-![Gráfico3 Barras](Output/grafica_3_barras.png)
+![Gráfico3 Barras](Generados/grafica_3_barras.png)
 
 Gráfico3 pastel 
-![Gráfico3 pastel](Output/grafica_3_pastel.png)
+![Gráfico3 pastel](Generados/grafica_3_pastel.png)
 
 Gráfico4 Barras
-![Gráfico4 Barras](Output/grafica_4_barras.png)
+![Gráfico4 Barras](Generados/grafica_4_barras.png)
 
 Gráfico4 pastel 
-![Gráfico4 pastel](Output/grafica_4_pastel.png)
+![Gráfico4 pastel](Generados/grafica_4_pastel.png)
 
 Gráfico5 Barras
-![Gráfico5 Barras](Output/grafica_5_barras.png)
+![Gráfico5 Barras](Generados/grafica_5_barras.png)
 
 Gráfico5 pastel 
-![Gráfico5 pastel](Output/grafica_5_pastel.png)
+![Gráfico5 pastel](Generados/grafica_5_pastel.png)
 
 Gráfico ejemplo Barras
-![Gráfico ejemplo Barras](Output/grafica_ejemplo_barras.png)
+![Gráfico ejemplo Barras](Generados/grafica_ejemplo_barras.png)
 
 Gráfico ejemplo pastel 
-![Gráfico ejemplo pastel](Output/grafica_ejemplo_pastel.png)
+![Gráfico ejemplo pastel](Generados/grafica_ejemplo_pastel.png)
 
 
--- main.py
+++ main.py
@@ -9,42 +9,43 @@ eje_x = ["A","B","C","D","E"]
 fig, ax = plt.subplots()
 ax.bar(eje_x, eje_y, color="blue", label="Datos de ejemplo")
 ax.set_title("Gráfica de ejemplo (barras)")
-fig.savefig("Output/grafica_ejemplo_barras.png", bbox_inches="tight")
+fig.savefig("Generados/grafica_ejemplo_barra.png", bbox_inches="tight")
 plt.close(fig)
 
 # Pastel estático
 fig, ax = plt.subplots()
 ax.pie(eje_y, labels=eje_x,autopct="%1.1f%%", startangle=90)
 ax.set_title("Gráfica de ejemplo (pastel)")
-fig.savefig("Output/grafica_ejemplo_pastel.png", bbox_inches="tight")
+fig.savefig("Generados/grafica_ejemplo_pastel.png", bbox_inches="tight")
 plt.close(fig)
 
-
+#Leer archivo excel
 archivo_excel="Datos/Datos_base_practica4.xlsx"
 contador=1
 while contador <=5:
-    data_hoja= pd.read_excel(archivo_excel, sheet_name=f"Hoja{contador}")
+    data_hoja= pd.read_excel(archivo_excel, sheet_name=f"Hoja{contador}", engine="openpyxl")
     print(f"Datos leídos de la hoja {contador}")
     
     eje_x = data_hoja.iloc[:, 0]  
-    eje_y = data_hoja.iloc[:, 1] 
+    eje_y_barras = data_hoja.iloc[:, 1] 
+    eje_y_pastel = data_hoja.iloc[:, 2] 
     
     # Gráfico de barras 
     fig, ax = plt.subplots()
-    ax.bar(eje_x, eje_y, color="green", label=f"Datos de Hoja {contador}")
+    ax.bar(eje_x, eje_y_barras, color="green", label=f"Datos de Hoja {contador}")
     ax.set_xlabel("Eje X")
     ax.set_ylabel("Eje Y")
     ax.set_xticklabels(eje_x, rotation=45)
     ax.set_title(f"Gráfica de la hoja {contador}(barras)")
-    fig.savefig(f"Output/grafica_{contador}_barras.png", bbox_inches="tight")
+    fig.savefig(f"Generados/grafica_{contador}_barras.png", bbox_inches="tight")
     plt.close(fig)
     
     # Gráfico de pastel 
     fig, ax = plt.subplots()
-    ax.pie(eje_y, labels=eje_x,autopct="%1.1f%%", startangle=90)
+    ax.pie(eje_y_pastel, labels=eje_x,autopct="%1.1f%%", startangle=90)
     ax.set_title(f"Gráfica de la hoja {contador}(pastel)")
-    fig.savefig(f"Output/grafica_{contador}_pastel.png", bbox_inches="tight")
+    fig.savefig(f"Generados/grafica_{contador}_pastel.png", bbox_inches="tight")
     plt.close(fig)
     
     contador=contador+1
-print("Gráficas de barra y pastel generadas y guardadas en la carpeta 'Output'.")
+print("Gráficas de barra y pastel generadas y guardadas en la carpeta 'Generados'.")
